413aa87b0277ecb1a215b286f08d3f9357b45482
Rémi Duraffort <ivoire@videolan.org>
2014-05-01 22:22:06 +0200

    Add a class for Outings

---
+++ src/org/randoamissecours/Outing.java
@@ -0,0 +1,36 @@
+package org.randoamissecours;
+
+import java.util.Date;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class Outing {
+	public String name;
+	public String description;
+	public int status;
+	public Date beginning;
+	public Date ending;
+	public Date alert;
+	public float latitude;
+	public float longitude;
+
+	public Outing(JSONObject object){
+		try {
+			this.name = object.getString("name");
+			this.description = object.getString("description");
+			this.status = object.getInt("status");
+			this.beginning = (Date) object.get("beginning");
+			this.ending = (Date)object.get("ending");
+			this.alert = (Date)object.get("alert");
+			this.latitude = (float)object.getDouble("latitude");
+			this.longitude = (float)object.getDouble("longitude");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+	public Outing(String name) {
+		this.name = name;
+	}
+}
