e9a7a62ee40789056ff918e05537346fb5f447e1
Rémi Duraffort <ivoire@videolan.org>
2014-05-12 22:06:14 +0200

    Add a sync intem to the menu
    The synchronization is not implemented for the moment.

--- src/org/randoamissecours/MainActivity.java
+++ src/org/randoamissecours/MainActivity.java
@@ -2,7 +2,11 @@ package org.randoamissecours;
 
 import java.util.ArrayList;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -10,11 +14,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 	public final static String OUTING_NAME = "org.randoamissecours.outing.name";
 	public final static String OUTING_DESCRIPTION = "org.randoamissecours.outing.description";
+	
+	public OutingsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +31,8 @@ public class MainActivity extends ActionBarActivity {
         // Create and populate the list of Outings
         ListView listView = (ListView) findViewById(R.id.list);
         ArrayList<Outing> outings = new ArrayList<Outing>();
-        final OutingsAdapter adapter = new OutingsAdapter(this, outings);
+        this.adapter = new OutingsAdapter(this, outings);
+        adapter.setNotifyOnChange(true);
         listView.setAdapter(adapter);
 
         // TODO: remove this code
@@ -59,9 +67,34 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_sync) {
+        	// Check that network is available
+        	ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        	NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        	if (networkInfo != null && networkInfo.isConnected()) {
+        		new SyncTask().execute();
+                return true;
+        	} else {
+        		Toast toast = Toast.makeText(this, "Network unavailable", Toast.LENGTH_SHORT);
+        		toast.show();
+        		return false;
+        	}
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class SyncTask extends AsyncTask<Void, Integer, ArrayList<Outing> > {
+    	protected ArrayList<Outing> doInBackground(Void... dummy) {
+    		System.out.println("Launching the task");
+    		ArrayList<Outing> outings = new ArrayList<Outing>();
+    		// TODO: sync with the server
+    		outings.add(new Outing("TODO"));
+    		return outings;
+    	}
+    	protected void onPostExecute(ArrayList<Outing> outings) {
+    		System.out.println("Task finished");
+    		adapter.clear();
+    		adapter.addAll(outings);
+    	}
     }
 }
